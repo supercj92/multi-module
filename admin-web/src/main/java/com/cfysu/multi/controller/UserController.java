b047f84f6f813343999bcf4f7011e696b3e8a5c8
supercj92 <ukd163@163.com>
2017-12-08 17:15:03 +0800

    基于开源的xxl搭建了管理后台

--- admin-web/src/main/java/com/cfysu/multi/controller/UserController.java
+++ admin-web/src/main/java/com/cfysu/multi/controller/UserController.java
@@ -6,8 +6,10 @@ import com.cfysu.multi.manager.UserManager;
 import com.cfysu.multi.util.Page;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
@@ -18,9 +20,11 @@ public class UserController {
     @Autowired
     private UserManager userManager;
 
-    @RequestMapping("/page")
-    @ResponseBody
-    public Page<User> page(Page<User> page){
-        return userManager.queryForPage(new UserQuery(), page);
+    @RequestMapping("/listUser")
+    public ModelAndView listUser(Page<User> page){
+        Page<User> userPage = userManager.queryForPage(new UserQuery(), page);
+        ModelAndView modelAndView = new ModelAndView("user");
+        modelAndView.addObject("page", userPage);
+        return modelAndView;
     }
 }
