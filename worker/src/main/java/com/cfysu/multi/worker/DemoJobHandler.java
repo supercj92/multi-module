2557b5666c8bdd0357b84f9a17b604ed316c519a
supercj92 <ukd163@163.com>
2017-12-14 18:00:55 +0800

    基于xxl搭建worker-worker可正常调度

---
+++ worker/src/main/java/com/cfysu/multi/worker/DemoJobHandler.java
@@ -0,0 +1,31 @@
+package com.cfysu.multi.worker;
+
+import com.xxl.job.core.biz.model.ReturnT;
+import com.xxl.job.core.handler.IJobHandler;
+import com.xxl.job.core.handler.annotation.JobHandler;
+import org.springframework.stereotype.Component;
+
+/**
+ * 任务Handler的一个Demo（Bean模式）
+ * 
+ * 开发步骤：
+ * 1、新建一个继承com.xxl.job.core.handler.IJobHandler的Java类；
+ * 2、该类被Spring容器扫描为Bean实例，如加“@Component”注解；
+ * 3、添加 “@JobHandler(value="自定义jobhandler名称")”注解，注解的value值为自定义的JobHandler名称，该名称对应的是调度中心新建任务的JobHandler属性的值。
+ * 4、执行日志：需要通过 "XxlJobLogger.log" 打印执行日志；
+ * 
+ * @author xuxueli 2015-12-19 19:43:36
+ */
+@JobHandler(value="demoJobHandler")
+@Component
+public class DemoJobHandler extends IJobHandler {
+
+	private int counter;
+
+	@Override
+	public ReturnT<String> execute(String... params) throws Exception {
+		System.out.println("hello world" + counter++);
+		return ReturnT.SUCCESS;
+	}
+	
+}
